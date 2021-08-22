cd01ff20ab3d0e268d491848012bcf1fe11a06e1
Curtis B <ceb2941@gmail.com>
2021-08-21 23:56:03 -0500

    testing screen buffer
    drawing on screen every second using the time library

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <windows.h>
 #include <iostream>
+#include <time.h>
 
 void screen_border(wchar_t*, int, int, int);
 
 int main(void)
 {
+    //variables for game clock
+    unsigned long long int seconds = 0;
+    clock_t timer;
+    float clock_millis;
     //variables for console screen buffer
     const int screen_width = 120;
     const int screen_height = 30;
@@ -13,15 +18,24 @@ int main(void)
     DWORD buffer_data = 0;
     wchar_t char_buffer[screen_mult];
     COORD buffer_coord = { 0, 0 };
-
+    int char_buffer_ind = 122;
     //setup console screen buffer
     screen_border(char_buffer, screen_width, screen_height, screen_mult);
     h_screen_buffer = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
     SetConsoleActiveScreenBuffer(h_screen_buffer);
     WriteConsoleOutputCharacter(h_screen_buffer, char_buffer, screen_mult, buffer_coord, &buffer_data);
-
+    timer = clock();
     //start main loop (exit if esc key is pressed)
     while (GetAsyncKeyState(VK_ESCAPE) == 0) {
+        clock_millis = ((float)(clock() - timer)) / CLOCKS_PER_SEC;
+        if (clock_millis >= 1) {
+            timer = clock();
+            seconds++;
+            char_buffer[char_buffer_ind] = 'x';
+            if (char_buffer_ind > screen_width - screen_height) {
+                char_buffer_ind++;
+            }
+        }
         WriteConsoleOutputCharacter(
             h_screen_buffer, char_buffer, screen_mult, buffer_coord, &buffer_data);
     }
